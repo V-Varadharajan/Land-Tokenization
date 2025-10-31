3f647c28d3e9d7f4ff7f659db45f73011582eb23
Varadharajan <116930167+V-Varadharajan@users.noreply.github.com>
2025-10-31 14:36:04 +0530

    Direct declaration

--- Frontend/client/src/lib/contract.ts
+++ Frontend/client/src/lib/contract.ts
@@ -1,6 +1,6 @@
 ﻿import LandTokenizationArtifact from "../../../LandTokenization.json";
 
-export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS || "0x90E7218a8F7F41364D933c260c770E00108C37a7";
+export const CONTRACT_ADDRESS = "0x90E7218a8F7F41364D933c260c770E00108C37a7";
 console.log("Using contract address:", import.meta.env.VITE_CONTRACT_ADDRESS);
 
 export const CONTRACT_ABI = LandTokenizationArtifact.abi;
